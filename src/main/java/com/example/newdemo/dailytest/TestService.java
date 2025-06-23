9ac97f52d5021e7ba9e40ea2a4a1122481b57e7b
Better-later <844418525@qq.com>
2025-06-23 23:05:14 +0800

    测试类编写

--- src/main/java/com/example/newdemo/dailytest/TestService.java
+++ src/main/java/com/example/newdemo/dailytest/TestService.java
@@ -1,14 +1,10 @@
 package com.example.newdemo.dailytest;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Service;
 
-//@Service
-@SpringBootTest
+@Service
 public class TestService {
 
-    @Test
     public void testMethod() {
         // 模拟抛出 IndexOutOfBoundsException
         int[] array = new int[5];
